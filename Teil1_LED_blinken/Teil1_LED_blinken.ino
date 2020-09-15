7570bc609593b06b280715ffd9a46d9e6284a051
christilehm10 <christian.lehmann1991@web.de>
2020-09-15 11:21:13 +0200

    LED
    define

--- Teil1_LED_blinken/Teil1_LED_blinken.ino
+++ Teil1_LED_blinken/Teil1_LED_blinken.ino
@@ -3,25 +3,25 @@
   letzte Änderung: 11.09.2020
   Version: 0.01
 
-  Hardware: gründe LED an PIN 3 und einen 220 Ohm Widerstand
+  Hardware: grüne LED an PIN 3 und einen 220 Ohm Widerstand
 
 */
 
-
+#define LED_gruen 3 // Variable für den Ausgangspin der gruenen LED
 
 void setup()
 {
-  pinMode(3, OUTPUT); //LED Grün
+  pinMode(LED_gruen, OUTPUT); //LED Grün
   Serial.begin(9600); //zur Kontrolle
 }
 
 void loop()
 {
   /*******************LED im sekundentakt an- und ausschalten*******************/
-  digitalWrite(3, HIGH);        //LED an
+  digitalWrite(LED_gruen, HIGH);        //LED an
   Serial.println("LED an" );
   delay(1000);                  //1s warten
-  digitalWrite(3, LOW);         //LED aus
+  digitalWrite(LED_gruen, LOW);         //LED aus
   Serial.println("LED aus" );
   delay(1000);
 
